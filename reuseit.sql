5bef40dca578ac22da0878d2d813c9d3a9564c87
cedrvn10 <brochettes40@outlook.fr>
2022-11-19 23:53:12 +0100

    Foreign key constraints in progress

--- reuseit.sql
+++ reuseit.sql
@@ -23,17 +23,18 @@ create table membres (
 -- lists table
 drop table if exists lists;
 create table lists (
-	lis_id int not null auto_increment primary key,
+	lis_id_member int not null auto_increment primary key,
+	lis_member int not null,
 	lis_name varchar(100) not null,
-	description text not null,
-	visbility varchar(100) not null
+	lis_description text not null,
+	lis_visbility varchar(100) not null
 )engine=innodb
 
 -- tasks table
 drop table if exists tasks;
 create table tasks (
 	tas_id int not null auto_increment primary key,
-	tas_idlist int not null,
+	tas_id_list int not null,
 	tas_name varchar(100) not null,
 	tas_description text not null,
     tas_importance int not null,
@@ -54,11 +55,14 @@ create table forums (
 -- subjects table
 drop table if exists subjects;
 create table subjects (
-	for_id int not null auto_increment primary key,
-	for_name varchar(500) not null,
-	for_description text not null,
-	for_date datetime not null,
-    for_status int not null,
+	sub_id int not null auto_increment primary,
+	sub_id_forum int not null,
+	sub_id_member int not null,
+	sub_id_task int,
+	sub_name varchar nto null,
+	sub_pinned int not null,
+	sub_status int not null,
+	sub_date datetime not null
 )engine=innodb;
 
 
@@ -66,8 +70,8 @@ create table subjects (
 drop table if exists messages;
 create table messages (
 	mes_id int not null auto_increment primary key,
-	mes_member int not null,
-	mes_subject int not null,
+	mes_id_member int not null,
+	mes_id_subject int not null,
 	mes_date datetime not null,
 	mes_content text not null,
     mes_status int not null,
@@ -78,10 +82,10 @@ create table messages (
 drop table if exists pmsubjects;
 create table pmsubjects (
 	pms_id int not null auto_increment primary key,
+	pms_id_author int not null,
 	pms_name varchar(500) not null,
 	pms_task int,
-	pms_id_author int not null,
-    mes_status int not null,
+    pms_status int not null,
 	pms_ip varchar(500) not null
 )engine=innodb;
 
@@ -115,11 +119,21 @@ create table pmnotrecieve (
 set foreign_key_checks =1;
 
 -- contraintes
-alter table tasks add constraint list_tasks foreign key (tas_list) references lists(lis_id);
-alter table produire add constraint treat_task foreign key (pro_auteur) references auteur(aut_id);
-alter table produire add constraint cs1 foreign key (pro_auteur) references auteur(aut_id);
-alter table produire add constraint cs1 foreign key (pro_auteur) references auteur(aut_id);
-alter table produire add constraint cs1 foreign key (pro_auteur) references auteur(aut_id);
+
+alter table lists add constraint author_lists foreign key (lis_id_member) references members(mem_id);
+alter table tasks add constraint list_of_task foreign key (tas_id_list) references lists(lis_id);
+
+alter table subjects add constraint subjects_forum foreign key (sub_id_forum) references forum(for_id);
+alter table subjects add constraint author_subject foreign key (sub_id_member) references members(mem_id);
+
+alter table messages add constraint mess_subject foreign key (mes_id_subject) references subject(sub_id);
+alter table messages add constraint mess_member foreign key (mes_id_member) references members(mem_id);
+
+alter table pmsubjects add constraint author_pm foreign key (pms_id_author) references members(mem_id);
+
+alter table pmanswers add constraint author_pm foreign key (pms_id_member) references members(mem_id);
+
+
 alter table produire add constraint cs1 foreign key (pro_auteur) references auteur(aut_id);
 alter table produire add constraint cs1 foreign key (pro_auteur) references auteur(aut_id);
 alter table produire add constraint cs1 foreign key (pro_auteur) references auteur(aut_id);
